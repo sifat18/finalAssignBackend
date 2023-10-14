becb35dbfd226a3127e93314f168aa0857c8c31c
mdsifat <ibnsifat@gmail.com>
2023-10-14 19:25:38 +0600

    initi

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app: Application = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
